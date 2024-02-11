decfda2b6f158c4ce5745ecffda6055fa9fc92be
goliathgrouper <vitalijsimon48@gmail.com>
2024-02-11 18:25:41 +0300

    Make grid resizable

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+const SIZE = 700;
+
 const container = document.querySelector('#container');
-let square;
-for (let i = 0; i < 256; i++) {
-    square = document.createElement('div');
-    square.style.border = '1px solid black';
-    square.style.padding = '20px';
-    square.style.backgroundColor = 'white';
-    container.appendChild(square);
+
+function getSquareSize(squareNumber) {
+    return `${(SIZE/squareNumber)-2}px`;
 }
+
+function generateGrid(n) {
+    let square;
+    for (let i = 0; i < n**2; i++) {
+        square = document.createElement('div');
+        square.style.border = '1px solid black';
+        square.style.width = getSquareSize(n);
+        square.style.height = getSquareSize(n);
+        square.style.backgroundColor = 'white';
+
+        container.appendChild(square);
+    }
+}
+
+generateGrid(16);
 
 container.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'green';
 });
+
+
+const changeGrid = document.querySelector('#grid-size');
+
+changeGrid.addEventListener('click', () => {
+    let grid = prompt();
+    while (grid < 0 || grid > 100) {
+        grid = prompt();
+    }
+    let oldGrid = document.querySelectorAll('#container > div');
+    oldGrid.forEach((square) => {
+        container.removeChild(square);
+    })
+    generateGrid(grid);
+})
