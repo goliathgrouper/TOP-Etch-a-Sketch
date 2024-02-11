ea84f0afbeaf3f6cc4bef28f5df66c358f3c8106
goliathgrouper <vitalijsimon48@gmail.com>
2024-02-11 17:17:32 +0300

    Make squares change background-color on mouseover

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ for (let i = 0; i < 256; i++) {
     square = document.createElement('div');
     square.style.border = '1px solid black';
     square.style.padding = '20px';
+    square.style.backgroundColor = 'white';
     container.appendChild(square);
 }
+
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'green';
+});
